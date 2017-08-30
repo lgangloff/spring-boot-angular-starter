b233c0b8761c9425cdbde1ed4f26d887754a0c97
lgangloff <loic.gangloff@pharmagest.com>
2017-08-30 20:24:56 +0200

    build dir

--- src/main/java/org/lgangloff/starter/config/WebConfigurer.java
+++ src/main/java/org/lgangloff/starter/config/WebConfigurer.java
@@ -12,12 +12,14 @@ import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Configuration of web application with Servlet 3.0 APIs.
  */
 @Configuration
-public class WebConfigurer implements ServletContextInitializer {
+public class WebConfigurer implements WebMvcConfigurer, ServletContextInitializer {
 
     private final Logger log = LoggerFactory.getLogger(WebConfigurer.class);
 
@@ -30,7 +32,13 @@ public class WebConfigurer implements ServletContextInitializer {
         servletContext.addListener(RequestContextListener.class);
         log.info("Web application fully configured");
     }
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath://dist/");
+	}
     
    
+    
 
 }
